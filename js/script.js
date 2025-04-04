9d61724e5da456515b6b53ca80e2ea47e1796d5c
Artem Nakonechnyi <artem301986@icloud.com>
2025-04-04 17:52:21 +0300

    initial

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const curDate = document.getElementById("currentDate");
+const mainForm = document.getElementById("mainForm");
+
+function displayDate() {
+  const today = new Date();
+
+  const formatter = new Intl.DateTimeFormat("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+
+  const formattedDate = formatter.format(today);
+  curDate.textContent = formattedDate;
+}
+
+displayDate();
+
+mainForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const formData = new FormData(mainForm);
+  formData.forEach((val, key) => {
+    console.log(`${key}: ${val}`);
+  });
+});
